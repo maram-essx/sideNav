0d5fd8887b5f7eaa9fd0d0a15d5563af25e8882d
maram-essx <maramessam381@gmail.com>
2024-09-01 15:21:24 +0300

    sidebar

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+interface ISidebarMenu {
+  icon?: string;
+  textField: { emoji?: string; text: string };
+  subItems?: string[];
+  isOpened?: boolean;
+}
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent {
+  loopedLis: ISidebarMenu[] = [];
+
+  ngOnInit(): void {
+    this.generateMenuItems();
+  }
+
+  generateMenuItems() {
+    this.loopedLis = [
+      {
+        textField: {
+          text: 'Link 1',
+        },
+      },
+      {
+        icon: 'fa-solid fa-chevron-down',
+        textField: {
+          emoji: '📚',
+          text: 'Category 2',
+        },
+        subItems: ['Link 3', 'Link 4'],
+        isOpened: false,
+      },
+      {
+        icon: 'fa-solid fa-chevron-down',
+        textField: {
+          emoji: '👍',
+          text: 'Category 3',
+        },
+        subItems: ['Link 5', 'Link 6'],
+        isOpened: false,
+      },
+    ];
+  }
+
+  toggleCollapse(item: ISidebarMenu, index: number) {
+    this.loopedLis.forEach((menuItem, index) => {
+      if (menuItem !== item && index != 0) {
+        menuItem.isOpened = false;
+        menuItem.icon = 'fa-solid fa-chevron-down';
+      }
+    });
+    if (index != 0) {
+      item.isOpened = !item.isOpened;
+      item.icon = item.isOpened
+        ? 'fa-solid fa-chevron-up'
+        : 'fa-solid fa-chevron-down';
+    }
+  }
+  
+}
